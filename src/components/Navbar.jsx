70944d2e81f584ad1b054bec666e70d5b5a0a681
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-05-25 15:51:18 +0530

    -changed logo -add navbar layout responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { setSearch } from "../redux/slices/SearchSlice.js";
 
 const Navbar = () => {
   const dispatch = useDispatch();
-  // const query = useRef();
 
   return (
-    <nav className="flex flex-col lg:flex-row justify-between mx-6 py-3 mb-6">
-      <div className="flex gap-2">
-        <img src="/src/assets/logo.png" alt="logo.png" className=" size-8" />
-        <h1 className="text-2xl font-bold">Tasty Bites</h1>
+    <nav className="flex flex-col md:flex-row justify-between mx-6 py-3 mb-6">
+      <div className="flex gap-2 md: justify-center">
+        <img
+          // src="/src/assets/Tasty Bites logo-modified.png"
+          src="/src/assets/Tasty Bites logo-modified.png"
+          alt="logo.png"
+          className=" w-12 h-12 mix-blend-multiply"
+        />
+        <h1 className="text-2xl font-bold my-2">Tasty Bites</h1>
       </div>
-      <div>
+      <div className="md:mt-0 mt-5">
         <input
           type="search"
           name="search"
           id=""
           placeholder="Search here..."
           autoComplete="off"
-          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw] lg:mt-0 sm:mt-5"
-          // ref={query}
+          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full md:w-[30vw]"
           onChange={(e) => dispatch(setSearch(e.target.value.toLowerCase()))}
         />
       </div>
