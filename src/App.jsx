d6d5b7351dadc79f28c035b46b522f8bc30af015
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-06-24 01:12:12 +0530

    feat: login, signup backend connection established

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
+import ResetPassword from "./pages/ResetPassword";
+import VerifyOtp from "./pages/VerifyOtp";
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
           element={<ProtectedRoute element={<Success />} />}
         />
         <Route path="/*" element={<Error />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ResetPassword />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
       </Routes>
     </BrowserRouter>
   );
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice.js";
 import CategorySlice from "./slices/CategorySlice.js";
 import SearchSlice from "./slices/SearchSlice.js";
+import AuthSlice from "./slices/AuthSlice.js";
 
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
     category: CategorySlice,
     search: SearchSlice,
+    auth: AuthSlice,
   },
 });
 
